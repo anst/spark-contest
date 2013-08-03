2993c2a89c4203bf04e3511543d0c02f77833c04
Andy Sturzu <andy@sturzu.org>
2013-08-03 12:38:32 -0500

    Added some fancy JS to forms, updated layout, removed extra welcomet text.

---
+++ views/assets/js/app.js
@@ -0,0 +1,22 @@
+$("#team").change(function() {
+	if($(this).val()=="null") {
+		$("#member3").fadeOut(400);
+		$("#member2").fadeOut(400);
+		$("#member1").fadeOut(400);
+	}
+	else if($(this).val()=="1") {
+		$("#member3").fadeOut(400);
+		$("#member2").fadeOut(400);
+		$("#member1").fadeIn(400);
+	}
+	else if($(this).val()=="2") {
+		$("#member3").fadeOut(400);
+		$("#member2").fadeIn(400);
+		$("#member1").fadeIn(400);
+	}
+	else {
+		$("#member3").fadeIn(400);
+		$("#member2").fadeIn(400);
+		$("#member1").fadeIn(400);
+	}
+});
