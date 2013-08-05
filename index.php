183a1121c8271b1afcdeef941bf3c7b8250122d1
Andy Sturzu <andy@sturzu.org>
2013-08-04 21:58:02 -0500

    Fixed some layout stuff, added /compile for testing of the compilation service. Currently extracts the data from the file and prettyprints it.

--- index.php
+++ index.php
@@ -20,49 +20,54 @@ $panel->route('/', function($panel) { //index router, check for login
 		"contest_name"=>contest_name,
 	]);
 });
+$panel->route('/compile', function($panel) {
+	return $panel->render("compile_temp.html",[
+		"title"=>title,
+		"contest_name"=>contest_name,
+	]);
+});
 $panel->route('/api/<string>', function($panel, $api_query) {
 	header('Content-Type: application/json'); //we're returning JSON data
 	if($api_query==="login") {
 
 	}
 	else if($api_query==="compile") {
-		$processname = md5(time() . getmypid() .rand(1,10));
+		$processname = md5(time() . getmypid() .rand(1,10)); //change this to md5(TeamNumber+Timestamp+rand(1,10000))
 		$data = "public class HelloWorld{public static void main(String []args){System.out.println(\"Hello World\\n## ###\"); try{Thread.sleep(100);}catch(Exception e){}}}";
 		$inputs = "";
 		$args = "";
 		$data = preg_replace('/(\r\n|\r|\n)/s',"\n",$data);
 
 		$proccess_safety = proc_safety();
-		if($proccess_safety!=="Success") return returnApiMessage(["query"=>$api_query,"error"=>$proccess_safety]);
+		if($proccess_safety!=="Success") return returnApiMessage(["error"=>$proccess_safety]);
 
 		register_shutdown_function('shutdown'); //before call to exit(), execute shutdown()
 		$source = preg_split("/(\n|;)/",$data);
 		$package = "";
 		$class = "";
 		foreach($source as $line){
-		  $line = trim($line);
-		  $pattern = "/^package\s+(.*)/";
-		  if(preg_match($pattern, $line, $matches )){
-		    $package = preg_replace('/\./', "/", $matches[1]);
-		  }
-		  $pattern = "/^public(\s+)class(\s+)(\w+).*/";
-		  if(preg_match($pattern, $line, $matches )){
-		    $class = trim($matches[3]);
-		  }
+			$line = trim($line);
+			$pattern = "/^package\s+(.*)/";
+			if(preg_match($pattern, $line, $matches )){
+				$package = preg_replace('/\./', "/", $matches[1]);
+			}
+			$pattern = "/^public(\s+)class(\s+)(\w+).*/";
+			if(preg_match($pattern, $line, $matches )){
+				$class = trim($matches[3]);
+			}
 		}
 
 		if(!strlen($class) ){
-		  echo "Error - At least one public class is required";
-		  exit(0);
+			echo "Error - At least one public class is required";
+			exit(0);
 		}
 		else if(strlen($package)) {
-		  echo "Error - Packages are not allowed";
-		  exit(0);
+			echo "Error - Packages are not allowed";
+			exit(0);
 		}
 		$compiled = compileProgram("/tmp/$processname/$class" . ".java", "/tmp/$processname/", "/tmp/$processname/$class" . ".class", $class, $inputs, $args, $processname, $data);
 		echo returnApiMessage([
-				"query"=>$api_query,
-				"response"=>$compiled
+				"success"=>$compiled
 			]
 		);
 		//if($compiled['exec']["output"]=="Hello World\n## ###\n")
--- views/assets/js/app.js
+++ views/assets/js/app.js
@@ -20,3 +20,32 @@ $("#team").change(function() { //display or hide the appropriate fields for the 
 		$("#member1").fadeIn(400);
 	}
 });
+
+function readBlob() {
+    var files = document.getElementById('files').files;
+    if (!files.length) {
+      alert('Please select a file!');
+      return;
+    }
+    var file = files[0];
+    var start = 0;
+    var stop = file.size - 1;
+
+    var reader = new FileReader();
+
+    reader.onloadend = function(evt) {
+      if (evt.target.readyState == FileReader.DONE) {
+      	$("#byte_content").fadeIn(600);
+        $("#byte_content").text(evt.target.result);
+        prettyPrint();
+        $("#upload").fadeOut(600);
+        $("#compile").fadeIn(600);
+      }
+    };
+
+    var blob = file.slice(start, stop + 1);
+    reader.readAsBinaryString(blob);
+}
+$("#upload").click(function() {
+	readBlob(0, 0);
+});
