8dcbf78ab30940bfe37083a588b3d00f7c24689a
Jonathan <jonathan@jonathanzong.com>
2013-10-19 21:59:23 -0500

    added scoreboard socketio handlers

--- app/node/server.js
+++ app/node/server.js
@@ -54,4 +54,74 @@ io.sockets.on('connection', function (socket) {
 			socket.emit('soft_refresh');
 		});
   	});
+
+  	// dumps table
+  	socket.on('scoreboard', function (data) {
+		division = data.division;
+		if(division && (division === 'Novice' || division === 'Advanced')) {
+			var m = mysql.createConnection({
+			  host     : 'localhost',
+			  user     : 'root',
+			  password : 'AwesomeSauce',
+			  database : 'thscs',
+			  port     : 8889,
+			  _socket: '/var/run/mysqld/mysqld.sock',
+			});
+			m.query("SELECT * FROM clarifications WHERE `division`='"+m.escape(division)+"' ORDER BY `score` DESC", function(err, result, fields) {
+				if(err) {
+		            m.end();
+		            console.error(err);
+		            return;
+		        } else if (result.length  > 0) {
+			        return socket.emit('scoreboard', toObject(result));
+			    }
+			});
+		} else socket.emit('scoreboard', {});
+  	});
+
+  	// dumps table
+  	socket.on('recalculate', function (data) {
+		team = data.team;
+		if(team && ) {
+			var m = mysql.createConnection({
+			  host     : 'localhost',
+			  user     : 'root',
+			  password : 'AwesomeSauce',
+			  database : 'thscs',
+			  port     : 8889,
+			  _socket: '/var/run/mysqld/mysqld.sock'
+			});
+			m.query("SELECT * FROM submissions WHERE `team`="+m.escape(team)+" ORDER BY `time`", function(err, result, fields) {
+				if(err) {
+		            m.end();
+		            console.error(err);
+		            return;
+		        } else if (result.length  > 0) {
+		        	var sum = 0;
+		        	var probs = {};
+		        	for (var i = 0; i < result.length; i++){
+		        		if(result[i]){
+		        			if(!probs[result[i].problem])
+		        				probs[result[i].problem] = 0;
+		        			if(probs[result[i].problem] <= 0){		        				
+			        			if(result[i].success === 'Yes'){
+			        				probs[result[i].problem] += 60;
+			        			}
+			        			else{
+			        				probs[result[i].problem] -= 5;
+			        			}
+		        			}
+		        		}
+		        	}
+		        	for(var x in probs){
+		        		if(probs.hasOwnProperty(x)){
+		        			if(probs[x] > 0)
+		        				sum += probs[x];
+		        		}
+		        	}
+			    	return socket.emit('recalculate', {score: sum});
+			    }
+			});
+		} else socket.emit('recalculate', {error:'needs team number'});
+  	});
 });
